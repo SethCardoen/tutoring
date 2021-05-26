4a6a8927908ce6834c42f9f3226d57491c9b95c3
SethCardoen <seth.cardoen@gmail.com>
2021-05-26 13:33:08 +0200

    -Making base of models and adding it to the apps in settings.py

--- mysite/basesite/models.py
+++ mysite/basesite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Education_level(models.Model):
+    diploma = models.CharField(max_length=200)
+
+class User(models.Model):
+    name = models.CharField('Name', max_length=200)
+    surname = models.CharField('Surname', max_length=200)
+    birthdate = models.DateField('enter your birthdate')
+    education_level = models.ForeignKey(Education_level, on_delete=models.CASCADE)
+
+
+
+
