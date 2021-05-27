569bdf0e5b5fe327db39ef8982e33c9373a11be3
SethCardoen <seth.cardoen@gmail.com>
2021-05-27 22:35:57 +0200

    - adding education levels - adding privileges - rebooting database

--- mysite/basesite/admin.py
+++ mysite/basesite/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, Education_level, level
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Education_level)
+admin.site.register(level)
--- mysite/basesite/models.py
+++ mysite/basesite/models.py
@@ -6,20 +6,30 @@ class Education_level(models.Model):
     diploma = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.Education_level_text
+        return self.diploma
 
-class User(models.Model):
+class level(models.Model):
+    rank = models.CharField('Priviliges', max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.rank
+
+class Profile(models.Model):
     name = models.CharField('Name', max_length=200)
     surname = models.CharField('Surname', max_length=200)
     birthdate = models.DateField('enter your birthdate')
     education_level = models.ForeignKey(Education_level, on_delete=models.CASCADE)
-    sign_in_date = models.DateField(auto_now=True)
+    sign_in_date = models.DateField(auto_now_add=True)
+    level = models.ForeignKey(level, on_delete=models.CASCADE, blank=True, null=True)
 
     def lenght_of_using_the_platform(self):
         loutp = date.today() - self.sign_in_date
 
     def __str__(self):
-        return self.User_text
+        return self.name
+
+
+
 
 
 
