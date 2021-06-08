7bf95721accb91d4c24f068596cd3d26e5342609
Kris Cardoen <kris.cardoen@porphyrio.com>
2021-06-08 17:16:06 +0200

    fixed move redirection

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
-from register import views as v
+
 
 urlpatterns = [
     path('<int:id>', views.index, name="index"),
     path('',views.home, name="home"),
-    path("register/", v.register, name="register"),
     path('create/', views.create, name="create"),
 
 ]
